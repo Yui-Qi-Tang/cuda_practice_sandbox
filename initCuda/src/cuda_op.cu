0d37a2cca828d3159f8d0fd03d03d96a2214f659
Yui Qi Tang <yqtang1222@gmail.com>
2018-09-21 15:31:53 +0800

    better relase pointer

--- initCuda/src/cuda_op.cu
+++ initCuda/src/cuda_op.cu
@@ -23,7 +23,7 @@ CudaChecker::CudaChecker(const int devices) {
 
 // Destructor
 CudaChecker::~CudaChecker() {
-	for(int i=_usedDevices - 1; i >= 0; i++) {
+	for(int i=_usedDevices - 1; i >= 0; i--) {
 		printf("relase devPtr[%d]", i);
 		free((_devProp+i));
 	}
