bca26043650c4fb632472fd2dd00a0e3cdabcaf2
Yui Qi Tang <yqtang1222@gmail.com>
2018-09-21 16:50:31 +0800

    amend

--- initCuda/src/main.cpp
+++ initCuda/src/main.cpp
@@ -2,9 +2,7 @@
 
 int main(int argc, char* argv[]) {
     CudaChecker checker(1);
-    // printf("Max numbers of device: %d\n", checker.getMaxDeviceCounts());
-    //checker.initCuda(1);
-    //checker.dumpDevicesProperty();
+    checker.dumpDevicesProperty();
     checker.setDevice(0);
     return 0;    
 }
